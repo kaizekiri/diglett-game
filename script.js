c2e5bbc9e750ddc03cf3685e227c2b50a95060f2
kaizekiri <mariusrosand@laptop-cka479b5.kea.local>
2016-11-29 12:12:48 +0100

    Structure

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict'
+var stage, preloadText, queue;
+function preload(){
+  stage = new createjs.Stage('canvas');
+  preloadText = new createjs.Text("Loading: ", "20px", "#000");
+  stage.addChild(preloadText);
+
+  queue = new createjs.LoadQueue(true);
+  queue.installPlugin(createjs.Sound);
+  queue.on('progress', progress);
+  queue.on('complete', complete);
+  queue.loadManifest([
+    'img/spritesheet.png',
+    {id:'blockJson', src:'json/diglett.json'}
+  ]);
+}
+
+function progress(e) {
+  preloadText.text=Math.round(e.progress*100)+'%';
+  stage.update();
+}
+
+function complete() {
+  setupLevel();
+  createjs.Ticker.setFPS(60);
+  createjs.Ticker.on('tick', tock);
+}
+
+function tock(e) {
+  stage.update(e);
+}
+
+function setupLevel() {
+  console.log("start game");
+  var sheet = new createjs.SpriteSheet(queue.getResult('blockJson'));
+  var hole = new createjs.Sprite(sheet, 'hole');
+  stage.addChild(hole);
+}
+
+window.addEventListener('load', preload);
